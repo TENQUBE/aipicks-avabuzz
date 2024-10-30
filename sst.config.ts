5ff45305905ef6140451fe209c251de1f940b42f
8dong <hyde981114@gmail.com>
2024-10-30 15:06:54 +0900

    chore: release workflows 추가 (#7)

---
+++ sst.config.ts
@@ -0,0 +1,35 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: 'buzzvil-thinkpool-webview',
+      removal: input?.stage === 'production' ? 'retain' : 'remove',
+      home: 'aws',
+      providers: {
+        aws: {
+          // version: '6.52.0',
+          region: 'ap-northeast-2'
+        }
+      }
+    }
+  },
+  async run() {
+    new sst.aws.Nextjs('BuzzvilThinkpoolWebview', {
+      domain: {
+        name: 'buzzvil-thinkpool.tenqube.com',
+        cert: 'arn:aws:acm:us-east-1:253030741903:certificate/80564239-ddf5-4e0c-9a11-e5fbb169aaab'
+      },
+      imageOptimization: {
+        memory: '512 MB'
+      },
+      environment: {
+        NEXT_PUBLIC_COUPANG_PARTNERS_API_ACCESS_KEY:
+          process.env.NEXT_PUBLIC_COUPANG_PARTNERS_API_ACCESS_KEY!,
+        NEXT_PUBLIC_COUPANG_PARTNERS_API_SECRET_KEY:
+          process.env.NEXT_PUBLIC_COUPANG_PARTNERS_API_SECRET_KEY!,
+        NEXT_PUBLIC_NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV!
+      }
+    })
+  }
+})
